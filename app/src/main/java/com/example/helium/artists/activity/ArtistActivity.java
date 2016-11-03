487c1a48ac37def35c6139f2b8753cc14607bc72
Daniel Konovalenko <ddkono@gmail.com>
2016-11-03 18:11:51 +0300

    Add description, genres tracks and albums count to Artist activity

--- app/src/main/java/com/example/helium/artists/activity/ArtistActivity.java
+++ app/src/main/java/com/example/helium/artists/activity/ArtistActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
 import com.example.helium.artists.R;
 import com.example.helium.artists.app.AppController;
 import com.example.helium.artists.model.Artist;
+
 
 public class ArtistActivity extends AppCompatActivity {
 
@@ -29,7 +31,26 @@ public class ArtistActivity extends AppCompatActivity {
         NetworkImageView imageView = (NetworkImageView) findViewById(R.id.bigImageView);
         imageView.setImageUrl(artist.getBigImageURL(), imageLoader);
 
+        // Setting genres
+        TextView genres = (TextView) findViewById(R.id.genres);
+        genres.setText(artist.getGenresString());
+
+        // Setting number of albums and tracks
+        TextView albumsTracksCount = (TextView) findViewById(R.id.albums_and_tracks_count);
+
+        String AlbumsCount = getResources().
+                getQuantityString(R.plurals.albums, artist.getAlbums(), artist.getAlbums());
+        String TracksCount = getResources().
+                getQuantityString(R.plurals.tracks, artist.getTracks(), artist.getTracks());
+
+        albumsTracksCount.setText(String.format("%s, %s", AlbumsCount, TracksCount));
+
+
+        // Setting description
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(artist.getDescription());
 
     }
+
 
 }
